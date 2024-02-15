31644f6f13893cc4015bf0b5565c5d77e90998ab
DhavalGhetiya <dghetiya@tibicle.com>
2024-02-15 12:32:49 +0530

    design-changes

---
+++ wp-content/themes/tibicle-theme/life -details-page.php
@@ -0,0 +1,24 @@
+<?php 
+
+/*
+*
+*
+ Template Name: life-details-page
+*
+*
+*/
+
+get_header();
+?>
+ <!-- page-content start -->
+
+  <!-- page-content start -->
+<?php 
+
+
+
+get_footer();
+
+?>  
+
+<?php
